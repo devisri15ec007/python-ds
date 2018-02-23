09626812efa769963ec5fa27b7f14a561fc9775b
devisri15ec007 <36723124+devisri15ec007@users.noreply.github.com>
2018-02-23 10:12:23 +0530

    Create check positive or negative.py

---
+++ check positive or negative.py
@@ -0,0 +1,7 @@
+a = float(input("Enter the number: "))
+if a > 0:
+   print("The given number is Positive")
+elif a == 0:
+   print("The given number is Zero")
+else:
+   print("The given number is Negative")
