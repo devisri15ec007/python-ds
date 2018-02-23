1d20f1e9a6a08e6806f345d6599ad32c7baebb65
devisri15ec007 <36723124+devisri15ec007@users.noreply.github.com>
2018-02-23 11:54:36 +0530

    Create alphabet or not.py

---
+++ alphabet or not.py
@@ -0,0 +1,5 @@
+Ch= str(input("Enter the Character:"))
+if(Ch >='a' and Ch <='z') or (Ch >='A' and Ch <='Z'):
+ print(Ch,"The given character is an Alphabet")
+else:
+  print(Ch,"The given character is not an Alphabet")
