7b90c8177d5838fd392b493b3bc5527a73058849
devisri15ec007 <36723124+devisri15ec007@users.noreply.github.com>
2018-02-23 12:31:09 +0530

    Create largest among 3 numbers.py

---
+++ largest among 3 numbers.py
@@ -0,0 +1,9 @@
+m=int(input("Enter the number:"))
+d=int(input("Enter the number:"))
+s=int(input("Enter the number:"))
+if(m >= s) and (m >= s):
+  print ("The largest number among given number is: m")
+elif(d >= m) and (d >= s ):
+  print ("The largest number among given number is: d")
+else:
+  print ("The largest number among given number is: s")
